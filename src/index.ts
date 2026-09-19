7912a4204540e7a1c3bf22250d300a5485a232a0
Vladimir Silenko <vladimir.silenko@mpstats.io>
2026-09-19 13:26:11 +0300

    feat: database_schema implement database schema

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { type Request, type Response } from 'express';
+import subjectsRouter from './routes/subjects'
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}))
+app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, ХУЙ!');
